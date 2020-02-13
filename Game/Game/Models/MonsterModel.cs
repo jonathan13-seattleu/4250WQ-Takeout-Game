cef9bf7c2e3b40dfca8dbe16fda5668b5c54d74d
Sasank Kanuri <SasankKanuri@users.noreply.github.com>
2020-02-13 15:24:32 -0800

    UPdated Monster Model comments to reflect Monsters.

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -4,17 +4,7 @@ using System;
 namespace Game.Models
 {
     /// <summary>
-    /// Item for the Game
-    /// 
-    /// The Items that a character can use, a Monster may drop, or may be randomly available.
-    /// The items are stored in the DB, and during game time a random item is selected.
-    /// The system supports CRUDi operatoins on the items
-    /// When in test mode, a test set of items is loaded
-    /// When in run mode the items from from the database
-    /// When in online mode, the items come from an api call to a webservice
-    /// 
-    /// When characters or monsters die, they drop items into the Items Pool for the Battle
-    /// 
+    /// Monster for the Game
     /// </summary>
     public class MonsterModel : BaseModel<MonsterModel>
     {
