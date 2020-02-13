fabdd27f30ae71d795052df67bcc3324b146d041
jonapang13 <pangeli8@seattleu.edu>
2020-02-13 10:10:54 -0800

    Added MonsterCreatePage to Monster folder

---
+++ Game/Game/Views/Monster/MonsterCreatePage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Create Item
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class MonsterCreatePage : ContentPage
+    {
+        // The item to create
+        GenericViewModel<MonsterModel> ViewModel { get; set; }
+
+        /// <summary>
+        /// Constructor for Create makes a new model
+        /// </summary>
+        public MonsterCreatePage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+
+            data.Data = new MonsterModel();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create";
+
+            //Need to make the SelectedItem a string, so it can select the correct item.
+            
+        }
+
+        /// <summary>
+        /// Save by calling for Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
+            }
+
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel the Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Range
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            LevelValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+
+
+
+    }
+}
