d6128e20fbba04beee15f29508e40710c10a273d
Sasank Kanuri <SasankKanuri@users.noreply.github.com>
2020-03-02 11:17:14 -0800

    Added auto assign button to the ItemsDropped Page

---
+++ Game/Game/Views/Battle/ItemsDroppedPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Main Game Page
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemsDroppedPage : ContentPage
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ItemsDroppedPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Quit the Battle
+        /// 
+        /// Quitting out
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+    }
+}
