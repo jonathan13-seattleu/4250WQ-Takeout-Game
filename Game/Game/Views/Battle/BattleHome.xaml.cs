f05544ff38eb5a9a9b605e28aabc8c15db885058
Sasank Kanuri <SasankKanuri@users.noreply.github.com>
2020-03-02 11:34:10 -0800

    Added a temp battle home page.

---
+++ Game/Game/Views/Battle/BattleHome.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+	/// <summary>
+	/// The Main Game Page
+	/// </summary>
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BattleHome : ContentPage
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public BattleHome()
+		{
+			InitializeComponent();
+		}
+
+		
+	}
+}
