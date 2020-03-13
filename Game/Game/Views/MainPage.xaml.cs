4193ecd754ab34e90f906bb650be7934119cf94c
Sasank Kanuri <SasankKanuri@users.noreply.github.com>
2020-03-12 21:03:32 -0700

    Updated Menus to point to the pickcharacterspage.

--- Game/Game/Views/MainPage.xaml.cs
+++ Game/Game/Views/MainPage.xaml.cs
@@ -42,7 +42,7 @@ namespace Game.Views
                         break;
 
                     case (int)MenuItemEnum.Battle:
-                        MenuPages.Add(id, new NavigationPage(new BattleHome()));
+                        MenuPages.Add(id, new NavigationPage(new PickCharactersPage()));
                         break;
 
                     case (int)MenuItemEnum.About:
